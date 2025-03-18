33be35fcec1c60df25b6b6a236975bd188bff566
Ayush <ayushkumarrai2@gmail.com>
2025-03-18 23:10:55 +0530

    Add solution for Codeforces problem in TheNewYearMeetingFriends.java

---
+++ src/CodeForces/TheNewYearMeetingFriends.java
@@ -0,0 +1,13 @@
+package CodeForces;
+import java.util.*;
+public class TheNewYearMeetingFriends {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[2] - arr[0]);
+    }
+}
