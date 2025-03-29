e33d12feaa3e2442113d47b0e86c4e21e42337ae
Ayush <ayushkumarrai2@gmail.com>
2025-03-29 22:22:03 +0530

    Add solution for Codeforces problem in RestoringThreeNumbers.java

---
+++ src/CodeForces/RestoringThreeNumbers.java
@@ -0,0 +1,16 @@
+package CodeForces;
+import java.util.*;
+public class RestoringThreeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[4];
+        for (int i = 0; i < 4; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int a = arr[3] - arr[0];
+        int b = arr[3] - arr[1];
+        int c = arr[3] - arr[2];
+        System.out.println(a + " " + b + " " + c);
+    }
+}
