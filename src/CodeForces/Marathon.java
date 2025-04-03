eab778e376c740895c9e3bcdcda31b846145dca3
Ayush <ayushkumarrai2@gmail.com>
2025-04-03 22:11:32 +0530

    Add solution for Codeforces problem in Marathon.java

---
+++ src/CodeForces/Marathon.java
@@ -0,0 +1,20 @@
+package CodeForces;
+import java.util.*;
+public class Marathon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        System.out.println(dp[n][m]);
+    }
+}
