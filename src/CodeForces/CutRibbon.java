8cfe61ace6af14a0a86862d22835538a48438665
Ayush <ayushkumarrai2@gmail.com>
2025-04-06 23:22:14 +0530

    Fix array declaration style in CutRibbon.java

--- src/CodeForces/CutRibbon.java
+++ src/CodeForces/CutRibbon.java
@@ -7,7 +7,7 @@ public class CutRibbon {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        int dp[] = new int[n + 1];
+        int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
         dp[0] = 0;
         for (int i = 0; i <= n; i++) {
