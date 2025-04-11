8fe15eed5621a428e96f8f8f93537e71590a0074
Ayush <ayushkumarrai2@gmail.com>
2025-04-11 12:11:20 +0530

    Add solution for Codeforces problem in Borze.java

---
+++ src/CodeForces/Borze.java
@@ -0,0 +1,22 @@
+package CodeForces;
+import java.util.*;
+public class Borze {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '.') {
+                result.append('0');
+            } else if (s.charAt(i) == '-') {
+                if (i + 1 < s.length() && s.charAt(i + 1) == '.') {
+                    result.append('1');
+                    i++;
+                } else {
+                    result.append('2');
+                }
+            }
+        }
+        System.out.println(result.toString());
+    }
+}
